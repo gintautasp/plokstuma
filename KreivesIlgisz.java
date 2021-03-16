0acaf246b1f972506f0aef78482ef01aa78573c8
Gintautas Petkevicius <g.petkevicius@inbox.lt>
2021-03-16 08:34:41 +0200

    pradedam prideti komentarus

--- KreivesIlgisz.java
+++ KreivesIlgisz.java
@@ -1,4 +1,9 @@
-
+/**
+* Programa skaičiuoja kreivės plokštumoje, kuri nusakoma taškais duomenų faile, ilgį.
+* @author dėstytojas
+* duomenu failo formatas artimas csv, skaičiai atskirti kableliais, .. 
+* .. pirmoje eilutėje taškų x reikšmės, antroje taškų y reikšmės
+*/
 import java.io.IOException; 
 
 import plokstuma.*;
@@ -8,17 +13,21 @@ public class KreivesIlgisz {
 	
 	public static void main(String[] args)  throws IOException  {
 		
+																							// programos pasisveikinimas							
+		
 		System.out.println( "Labas Java pasauli .." );
-		System.out.println( "Skaiciuosim kreives ilgi:" );	
-		
-		KonsolesDialogas konsoles_dialogas = new KonsolesDialogas();
+		System.out.println( "Skaiciuosim kreives ilgi:" );
+																							//   organizuojamas konsolės dialogas, ..
+																							// .. kuriuo įvedamas failo vardas, ..
+		KonsolesDialogas konsoles_dialogas = new KonsolesDialogas();									// .. arba nustatomas vardas  kreives_taskai.csv, ..
+																						// .. pagal nutylejima ir perduodamas priminimo,
+		String vardas_failo = konsoles_dialogas.ivestiReiksme (" kreives tasku failas", "kreives_taskai.csv" );	      			//  .. ką įvesti - " kreivės taškų failas "
+																						
+		KreiveIsFailoXXYY kreive_is_failo_xx_yy = new KreiveIsFailoXXYY ( vardas_failo ); 						// kuriams kreivės skaitymo iš failo objektas ..
+		Kreive kreive = kreive_is_failo_xx_yy.gautiKreive();											// .. ir pasiimamas Kreives objektas, kuriame yra ..
+																											//  .. Taskai masyvas
 
-		String vardas_failo = konsoles_dialogas.ivestiReiksme (" kreives tasku failas", "kreives_taskai.csv" );	
-		
-		KreiveIsFailoXXYY kreive_is_failo_xx_yy = new KreiveIsFailoXXYY ( vardas_failo ); 
-		Kreive kreive = kreive_is_failo_xx_yy.gautiKreive();
-
-		System.out.println ( kreive.toString() );
-		System.out.println ( "Kreives ilgis: " + kreive.ilgis() );
+		System.out.println ( kreive.toString() );																	// parodomi kreivės taškai
+		System.out.println ( "Kreives ilgis: " + kreive.ilgis() );												// ir parašomas suskaičiuotas kreivės ilgis
 	}	
 }
--- konsole/KonsolesDialogas.java
+++ konsole/KonsolesDialogas.java
@@ -4,16 +4,28 @@
 	import java.io.BufferedReader; 
 	import java.io.IOException; 
 	import java.io.InputStreamReader; 
-
+	
+	/**
+	* Klasė dialogo su vartotoju konsolėje oranizavimui.
+	*/
 	public class KonsolesDialogas  {
 		
-		public BufferedReader reader;
+		public BufferedReader reader;												// skaitymo iš failo tarpinis objektas.
 		
+		/**
+		* konstruktorius be parametrų, sukuria skaitymo iš failo tarpinį objektą
+		*/
 		public KonsolesDialogas() {
 			
 			reader = new BufferedReader ( new InputStreamReader (System.in ) ); 			
 		}
 		
+		/**
+		* iš konsolės įvedama teksto eilutė, prieš tai išvedant priminimą ir reikšmę kuri bus gražinama pagal nultylėjimą, paspaudus tik Enter.
+		*@param String priminimas
+		*@param String nutylint
+		*@return String
+		*/
 		public String ivestiReiksme ( String priminimas, String nutylint ) throws IOException {
 		
 			System.out.print ( priminimas + " [" + nutylint + "] ? " );	
